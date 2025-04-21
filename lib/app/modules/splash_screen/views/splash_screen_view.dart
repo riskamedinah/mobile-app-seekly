83c214c03e4406001aea4793f2df3e37d8948230
reinksir <166875533+reinksir@users.noreply.github.com>
2025-04-21 22:29:27 +0700

    first commit

---
+++ lib/app/modules/splash_screen/views/splash_screen_view.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SplashScreenView extends StatefulWidget {
+  const SplashScreenView({super.key});
+
+  @override
+  State<SplashScreenView> createState() => _SplashScreenViewState();
+}
+
+class _SplashScreenViewState extends State<SplashScreenView> 
+{
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(Duration(seconds: 3), () {
+      Get.offNamed('/login');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    backgroundColor: Colors.white,
+      body: Center(
+        child: Text(
+          'Seekly',
+          style: GoogleFonts.montserrat(
+          fontSize: 70,
+          fontWeight: FontWeight.w800,
+          color: Color(0xFFED652A)
+          ),
+        ),
+      ),
+    );
+  }
+}
